d5a22380daac720dd29f203feb804eb176107f91
Carolina Hernandez <carolinahernandez@cucunver.com>
2025-02-06 23:36:36 +0000

    login router

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '@/views/HomeView.vue'
+import Login from '@/views/Login.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,9 +8,10 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-			children: [
-				{ path: '/', name: 'homee', component: HomeView}
-			]
+      children: [
+        { path: '/', name: 'homee', component: HomeView },
+        { path: '/login', name: 'login', component: Login },
+      ],
     },
   ],
 })
