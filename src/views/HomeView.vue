b234d0b4f2780b09cc22de2c05afc75eefd435ec
Carolina Hernandez <carolinahernandez@cucunver.com>
2025-02-06 23:34:26 +0000

    dashboard with categories

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,22 @@
 <template>
-  <main>
-  </main>
+  <div class="w-full h-full relative px-8 pt-10">
+    <p class="font-bold text-[30px]">{{ $t("dashboardTitle") }}</p>
+    <CategoriesList :categories="store.categories" :activeCategory="store.activeCategory" @set-category="store.setActiveCategory" />
+  </div>
 </template>
 
 <script setup>
+import { onMounted, onBeforeMount } from 'vue'
+import { useStore } from '@/store'
+import Icon from '@/components/common/Icon.vue'
+import CategoriesList from '@/components/categories/CategoriesList.vue'
+import ServiceCard from '@/components/services/Card.vue'
+
+const store = useStore();
+
+onBeforeMount(async () => {
+  await store.fetchServices()
+  await store.fetchCategories()
+})
 
 </script>
