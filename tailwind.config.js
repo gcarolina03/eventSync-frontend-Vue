0c1a6dd60e6b8514624e4330ee306be59ffe5b34
Carolina Hernandez <carolinahernandez@cucunver.com>
2025-02-07 15:02:39 +0000

    add fontSizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,17 @@ export default {
         dark: '#312a82',
         text: '#150D2C',
       },
+      fontSize: {
+        'xs': '0.5rem', // 8px
+        'sm': '0.625rem', // 10px
+        'base': '0.75rem', // 12px
+        'lg': '0.875rem', // 14px
+        'xl': '1rem', // 16px
+        '2xl': '1.125rem', // 18px
+        '3xl': '1.25rem', // 20px
+        '4xl': '1.5rem', // 24px
+        '5xl': '1.875rem', // 30px
+      },
     },
   },
   plugins: [],
