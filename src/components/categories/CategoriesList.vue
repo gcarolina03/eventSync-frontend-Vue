88b43de6b32d1fb929f16721aa03a5ae0b5f03f6
Carolina Hernandez <carolinahernandez@cucunver.com>
2025-02-09 20:01:03 +0000

    fix style categories list

--- src/components/categories/CategoriesList.vue
+++ src/components/categories/CategoriesList.vue
@@ -6,7 +6,7 @@
         {{ activeCategory ? $t("categoriesList." + activeCategory) : $t("categoriesList.all") }}
       </button>
       <div v-if="isOpen" class="mt-2 bg-white shadow rounded-lg">
-        <ul>
+        <ul class="">
           <li @click="$emit('set-category', 'all')" :class="['py-4 font-semibold hover:bg-gray-100 flex gap-6 items-center px-6 text-sm lg:text-base', activeCategory == 'all' ? 'bg-gray-100' : 'cursor-pointer']">
             {{ $t("categoriesList.all") }}
           </li>
@@ -19,12 +19,12 @@
     </div>
 
     <!-- Menú horizontal para pantallas grandes -->
-    <ul class="hidden md:inline-flex bg-white shadow mt-6 rounded-lg overflow-x-auto">
+    <ul class="hidden md:inline-flex md:items-center bg-white shadow mt-6 rounded-lg overflow-x-auto">
       <li @click="$emit('set-category', 'all')" :class="['py-4 font-semibold border-r-2 hover:bg-gray-100 flex gap-6 items-center px-6', activeCategory == 'all' ? 'bg-gray-100' : 'cursor-pointer', 'text-base lg:text-xl']">
         {{ $t("categoriesList.all") }}
       </li>
       <li v-for="category in categories" :key="category._id" @click="$emit('set-category', category._id)" :class="['py-4 font-semibold border-r-2 hover:bg-gray-100 flex flex-col gap-2 xl:gap-6 xl:flex-row items-center px-6', activeCategory == category._id ? 'bg-gray-100' : 'cursor-pointer', 'text-base lg:text-xl']">
-        <Icon :icon="category.icon" />
+          <Icon :icon="category.icon" :className="['h-6 flex items-center', 'xl:h-full']"/>
         <span class="hidden xl:inline">{{ $t("categoriesList." + category.title )}}</span>
       </li>
     </ul>
