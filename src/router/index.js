7229df60b9026b00cea67e46b529af49e283c4ea
Carolina Hernandez <carolinahernandez@cucunver.com>
2025-02-09 19:14:57 +0000

    create views

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import Login from '@/views/Login.vue'
 import Signup from '@/views/Signup.vue'
+import EventsView from '@/views/EventsView.vue'
+import ServicesView from '@/views/ServicesView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,8 +13,29 @@ const router = createRouter({
       name: 'home',
       children: [
         { path: '/', name: 'homee', component: HomeView },
-        { path: '/login', name: 'login', component: Login },
-        { path: '/signup', name: 'signup', component: Signup },
+        {
+          path: '/auth',
+          children: [
+            { path: '/auth/login', name: 'login', component: Login },
+            { path: '/auth/signup', name: 'signup', component: Signup },
+          ],
+        },
+        {
+          path: '/events',
+          name: 'events',
+          component: EventsView,
+          children: [
+            { path: '/events/:id', name: 'event', component: EventsView },
+          ],
+        },
+        {
+          path: '/services',
+          name: 'services',
+          component: ServicesView,
+          children: [
+            { path: '/services/:id', name: 'service', component: ServicesView },
+          ],
+        },
       ],
     },
   ],
