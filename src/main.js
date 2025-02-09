b69e3426f4eba4eefa54eac65801688ce8c60f70
Carolina Hernandez <carolinahrndez3@gmail.com>
2025-02-09 12:37:23 +0000

    move i18n config

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
-import i18n from '../i18n.config.js'
+import i18n from '@/i18n.config.js'
 
 const app = createApp(App)
 
