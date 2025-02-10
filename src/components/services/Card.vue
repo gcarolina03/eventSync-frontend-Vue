7747f0bd23386309734eff742755caf8b69610ac
Carolina Hernandez <carolinahernandez@cucunver.com>
2025-02-10 12:30:59 +0000

    add Card and ServicesView components to use props for data handling and improve layout; add filtering for services by category

--- src/components/services/Card.vue
+++ src/components/services/Card.vue
@@ -1,47 +1,68 @@
 <template>
-  <div class="bg-white border border-gray-200 rounded-lg relative h-[400px] w-[300px] shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]">
+  <div
+    class="bg-white border border-gray-200 rounded-lg relative h-[400px] w-[300px] shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]">
     <div class="bg-[#9CC0FA] rounded-t-lg relative h-[180px]">
-      <div v-if="editMode" @click="edit(data)" class="cursor-pointer z-10 absolute top-[-10px] left-[-10px] gap-2 h-8 py-2 text-sm rounded-lg font-bold bg-gray-300 hover:bg-gray-400 text-gray-700 px-4">
+      <div v-if="props.editMode" @click="props.edit(props.data)"
+        class="cursor-pointer z-10 absolute top-[-10px] left-[-10px] gap-2 h-8 py-2 text-sm rounded-lg font-bold bg-gray-300 hover:bg-gray-400 text-gray-700 px-4">
         <Icon name="pencil" />
       </div>
-      <div class="px-3 text-sm py-1 bg-gray-600 text-white font-bold top-2 left-2 rounded-lg absolute opacity-80">{{ data.categoryId.title }}</div>
-      <div class="px-3 text-lg py-1 bg-neutral-100 text-black font-bold bottom-2 right-2 rounded-lg absolute opacity-80">{{ data.price }} €</div>
-      <img class="rounded-t-lg w-full h-full object-cover mx-auto" :src="data.img_url" alt="" />
+      <div class="px-3 text-sm py-1 bg-gray-600 text-white font-bold top-2 left-2 rounded-lg absolute opacity-80">{{
+        props.data.categoryId.title }}</div>
+      <div
+        class="px-3 text-lg py-1 bg-neutral-100 text-black font-bold bottom-2 right-2 rounded-lg absolute opacity-80">{{
+          props.data.price }} €</div>
+      <img class="rounded-t-lg w-full h-full object-cover mx-auto" :src="props.data.img_url" alt="" />
     </div>
     <div class="p-5">
-      <h5 class="text-lg font-bold tracking-tight">{{ data.title }}</h5>
-      <div class="text-gray-600 mb-4">
-        <!-- <span v-if="location.length > 0">{{ location[6].long_name }} • {{ location[2].long_name }}</span>
+      <h5 class="text-lg font-bold tracking-tight">{{ props.data.title }}</h5>
+      <!-- <div class="text-gray-600 mb-4">
+        <span v-if="location.length > 0">{{ location[6].long_name }} • {{ location[2].long_name }}</span>
         <div class="group inline-flex cursor-pointer text-sm ml-2 relative text-black font-bold rounded-lg">
           <Icon name="map" />
           <div class="opacity-0 w-28 flex items-center justify-center bg-black rounded-lg p-2 absolute group-hover:opacity-100 bottom-0 ml-8 pointer-events-none z-[10]">
             <Maps :latitude="data.latitude" :longitude="data.longitude" />
           </div>
-        </div> -->
-      </div>
-      <div v-if="data.min_capacity && data.max_capacity" class="flex gap-2 items-center text-gray-600">
+        </div>
+      </div> -->
+      <div v-if="props.data.min_capacity && props.data.max_capacity" class="flex gap-2 items-center text-gray-600">
         <Icon name="user" />
-        <p class="font-normal">from {{ data.min_capacity }} to {{ data.max_capacity }}</p>
+        <p class="font-normal">from {{ props.data.min_capacity }} to {{ props.data.max_capacity }}</p>
       </div>
-      <div v-if="data.start_time && data.end_time" class="flex gap-2 items-center text-gray-600">
+      <div v-if="props.data.start_time && props.data.end_time" class="flex gap-2 items-center text-gray-600">
         <Icon name="clock" />
-        <p class="font-normal">{{ data.start_time }}h - {{ data.end_time }}h</p>
+        <p class="font-normal">{{ props.data.start_time }}h - {{ props.data.end_time }}h</p>
       </div>
     </div>
-    <div v-if="currentPath === '/services'" class="absolute right-4 bottom-3 cursor-pointer inline-flex items-center px-3 py-2 text-sm font-bold text-center text-white bg-light rounded-lg hover:bg-dark focus:ring-4 focus:outline-none focus:ring-dark" @click="requestTo(data)">
-      {{ $t("servicesCard.request")}}
+    <div v-if="currentPath === '/services'"
+      class="absolute right-4 bottom-3 cursor-pointer inline-flex items-center px-3 py-2 text-sm font-bold text-center text-white bg-light rounded-lg hover:bg-dark focus:ring-4 focus:outline-none focus:ring-dark"
+      @click="props.requestTo(props.data)">
+      {{ $t("servicesCard.request") }}
     </div>
     <div class="absolute bottom-3 left-5 flex gap-4">
-      <div @click="giveReviewService('up', data._id)" class="text-green-600 gap-1 font-bold flex text-lg items-center" :class="user ? 'cursor-pointer' : ''">
-        <Icon name="thumbs-up" v-if="user && reviewsCount().userUp" />
-        <Icon name="thumbs-up-fill" v-else-if="user && !reviewsCount().userUp" />
-        <Icon name="thumbs-up" v-else />
+      <div @click="handleReviewService('up', props.data._id)" class="text-green-600 gap-1 font-bold flex text-lg items-center"
+        :class="store.user ? 'cursor-pointer' : ''">
+        <template v-if="store.user && reviewsCount().userUp">
+          <Icon name="thumbs-up" />
+        </template>
+        <template v-else-if="store.user && !reviewsCount().userUp">
+          <Icon name="thumbs-up-fill" />
+        </template>
+        <template v-else>
+          <Icon name="thumbs-up" />
+        </template>
         <span>{{ reviewsCount().up }}</span>
       </div>
-      <div @click="giveReviewService('down', data._id)" class="text-red-600 gap-1 font-bold flex text-lg items-center" :class="user ? 'cursor-pointer' : ''">
-        <Icon name="thumbs-down" v-if="user && reviewsCount().userDown" />
-        <Icon name="thumbs-down-fill" v-else-if="user && !reviewsCount().userDown" />
-        <Icon name="thumbs-down" v-else />
+      <div @click="handleReviewService('down', props.data._id)" class="text-red-600 gap-1 font-bold flex text-lg items-center"
+        :class="store.user ? 'cursor-pointer' : ''">
+        <template v-if="store.user && reviewsCount().userDown">
+          <Icon name="thumbs-down" />
+        </template>
+        <template v-else-if="store.user && !reviewsCount().userDown">
+          <Icon name="thumbs-down-fill" />
+        </template>
+        <template v-else>
+          <Icon name="thumbs-down" />
+        </template>
         <span>{{ reviewsCount().down }}</span>
       </div>
     </div>
@@ -49,27 +70,24 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
 import { useStore } from '@/store'
+/* COMPONENTS */
 import Icon from '@/components/common/Icon.vue'
 
-
-defineProps({
+const props = defineProps({
   data: Object,
   editMode: Boolean,
   edit: Function,
   requestTo: Function,
 })
 
-const currentPath = useRoute().path
-
-const { user, giveReview, updateReview } = useStore()
-
-const location = ref('')
+const route = useRoute()
+const store = useStore()
+const currentPath = route.path
 
 const reviewsCount = () => {
-  const dataReview = data.serviceReviews
+  const dataReview = props.data.serviceReviews
   const reviews = {
     up: 0,
     down: 0,
@@ -77,20 +95,20 @@ const reviewsCount = () => {
     userDown: false
   }
   dataReview.forEach((review) => {
-    review.thumb === 'up' ? reviews.up++ : reviews.down++
-    if (props.user && review.userId === props.user._id) {
-      if (review.thumb === 'up') reviews.userUp = true
-      if (review.thumb === 'down') reviews.userDown = true
+    review.thumb == 'up' ? reviews.up++ : reviews.down++
+    if (store.user && review.userId === store.user._id) {
+      if (review.thumb == 'up') reviews.userUp = true
+      if (review.thumb == 'down') reviews.userDown = true
     }
   })
   return reviews
 }
 
-const giveReviewService = async (thumb, service) => {
-  if (props.user) {
-    const res = await giveReview(service, thumb)
+const handleReviewService = async (thumb, service) => {
+  if (store.user) {
+    const res = await store.giveReview(service, thumb)
     if (res.hasOwnProperty('reviewId')) {
-      await updateReview(res.reviewId, thumb)
+      await sotre.updateReview(res.reviewId, thumb)
     }
   }
 }
--- src/views/ServicesView.vue
+++ src/views/ServicesView.vue
@@ -1,6 +1,101 @@
 <template>
-  
+  <div class="md:w-[95%] px-4 md:px-0 flex-grow py-8 mx-auto">
+    <div class="flex flex-col md:grid md:grid-cols-12 gap-6">
+      <!-- Sidebar de filtros -->
+      <div class="md:min-w-15 md:col-span-1 xl:col-span-3">
+        <div class="flex flex-col gap-2 mt-3 text-base bg-white shadow rounded-lg">
+          <CategoriesList :categories="store.categories" direction="vertical" />
+        </div>
+      </div>
+
+
+      <!-- Sección de servicios -->
+      <div class="md:col-span-11 xl:col-span-9">
+        <p class="font-bold text-[30px]">{{ $t("ourServices") }} ({{ filteredServices.length }})</p>
+        <p>
+          {{ $t("servicesDescription") }}
+        </p>
+        <hr class="my-5 h-0.5 border-t-0 bg-gray-500 opacity-20" />
+        <div class="mt-4 flex flex-wrap gap-8">
+          <template v-if="filteredServices.length == 0">
+            <NoServices  />
+          </template>
+          <template v-else>
+            <Card
+              v-for="service in filteredServices"
+              :key="service._id"
+              :data="service"
+              :update="handleReload"
+              :user="userLog"
+              :requestTo="handleServiceTo"
+            />
+          </template>
+        </div>
+      </div>
+    </div>
+
+    <!-- Formulario de solicitud de servicio -->
+    <!-- <Blur v-if="showForm">
+      <SelectEventForm :handleForm="handleForm" :service="serviceTo" />
+    </Blur> -->
+  </div>
 </template>
 
 <script setup>
+import { ref, onBeforeMount, computed } from 'vue'
+import { useRouter } from 'vue-router'
+import { useStore } from '@/store';
+/* COMPONENTS */
+import CategoriesList from '@/components/categories/CategoriesList.vue'
+import NoServices from '@/components/services/NoServices.vue'
+import Card from '@/components/services/Card.vue'
+/* import SelectEventForm from '@/components/requests/SelectEventForm.vue'
+import Blur from '@/components/common/Blur.vue' */
+
+
+const router = useRouter()
+const store = useStore()
+const activeCategory = ref(store.activeCategory)
+
+onBeforeMount(async () => {
+  await store.fetchCategories()
+  await store.fetchServices()
+})
+
+const services = ref(store.services)
+const reload = ref(false)
+const userLog = ref(null)
+const showForm = ref(false)
+const serviceTo = ref(null)
+
+
+// Alternar formulario de solicitud de servicio
+const handleForm = () => {
+  showForm.value = !showForm.value
+}
+
+// Recargar servicios
+const handleReload = () => {
+  reload.value = true
+}
+
+// Manejo de servicio seleccionado
+const handleServiceTo = (data) => {
+  if (userLog.value) {
+    serviceTo.value = data
+    showForm.value = true
+  } else {
+    router.push('/login')
+  }
+}
+
+
+// Filtrar servicios por categoría
+const filteredServices = computed(() => {
+  if (activeCategory.value == '-1') {
+    return services.value
+  }
+  return services.value.filter((service) => service.categoryId._id == activeCategory.value)
+})
+
 </script>
