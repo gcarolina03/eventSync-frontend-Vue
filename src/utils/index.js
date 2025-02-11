b2be16b779bc1395418c64c31ddfb35d1da9a60a
Carolina Hernandez <carolinahernandez@cucunver.com>
2025-02-11 08:39:05 +0000

    add formatDate function

--- src/utils/index.js
+++ src/utils/index.js
@@ -6,4 +6,23 @@ const getDefaultAvatarUrl = () => {
   return 'https://res.cloudinary.com/dhveca8ba/image/upload/v1689175163/jvpcce9vmgjqjzqcc8ec.jpg'
 }
 
-export { isValidFileType, getDefaultAvatarUrl }
+const formatDate = (date) => {
+  if (!date) return
+
+  const dateString = date.toString()
+  const year = dateString.slice(0, 4)
+  const month = dateString.slice(4, 6)
+  const day = dateString.slice(6)
+
+  const parsedDate = new Date(`${year}-${month}-${day}`)
+  const formatter = new Intl.DateTimeFormat('en-US', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+  })
+
+  const [{ value: monthValue }, , { value: dayValue }, , { value: yearValue }] = formatter.formatToParts(parsedDate)
+  return `${monthValue}-${dayValue}-${yearValue}`
+}
+
+export { isValidFileType, getDefaultAvatarUrl, formatDate }
