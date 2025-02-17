8eb0a37fa0bca9f48468a115444ad0e6908dd77f
Carolina Hernandez <carolinahrndez3@gmail.com>
2025-02-17 22:25:47 +0000

    mejorar el estilo de los botones añadiendo clases de flex y centrado

--- src/components/common/Button.vue
+++ src/components/common/Button.vue
@@ -44,10 +44,10 @@ const buttonClass = computed(() => {
       baseClass = 'text-white sm:border cursor-pointer font-bold border-white rounded-xl px-5 py-2.5 hover:bg-dark hover:border-dark';
       break;
     case 'white':
-			baseClass = "text-secondary border cursor-pointer transition-all border-white bg-white font-bold rounded-xl px-5 py-2.5 hover:bg-dark hover:border-dark hover:text-white";
+			baseClass = "text-secondary border cursor-pointer transition-all border-white bg-white font-bold rounded-xl px-5 py-2.5 hover:bg-dark hover:border-dark hover:text-white flex items-center justify-center";
       break;
     case 'purple':
-			baseClass = 'w-full bg-secondary text-white font-bold py-3 rounded-xl transition-all hover:bg-dark focus:ring-2 focus:ring-secondary focus:ring-opacity-50 cursor-pointer';
+			baseClass = 'w-full bg-secondary text-white font-bold py-2.5 rounded-xl transition-all hover:bg-dark focus:ring-2 focus:ring-secondary focus:ring-opacity-50 cursor-pointer flex items-center justify-center';
       break;
     case 'menu':
       baseClass = 'block py-2 font-bold text-gray-400 hover:text-light'
@@ -56,7 +56,7 @@ const buttonClass = computed(() => {
       baseClass = 'block py-2 font-bold text-white cursor-default'
       break;
     default:
-      baseClass = 'bg-gray-300 text-white py-2 px-5 rounded-xl';
+			baseClass = 'bg-gray-300 text-white py-2.5 px-5 rounded-xl flex items-center justify-center';
   }
 
   return `${baseClass} ${props.additionalClass}`;
