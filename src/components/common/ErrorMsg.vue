4450d1c3ee1417c509691158169fd1573d3e58b5
Carolina Hernandez <carolinahernandez@cucunver.com>
2025-02-17 13:22:27 +0000

    ajustar el estilo del icono de cierre

--- src/components/common/ErrorMsg.vue
+++ src/components/common/ErrorMsg.vue
@@ -1,9 +1,8 @@
 <template>
   <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
-    <strong class="font-bold">Error! </strong>
     <span class="block sm:inline"> {{ message }}.</span>
-    <span class="absolute top-0 right-0 px-4 py-3" @click="hideError">
-      <Icon icon="x-mark" class="fill-current h-6 w-6 text-red-500 cursor-pointer" title="Close" />
+    <span class="absolute top-2 right-2" @click="hideError">
+      <Icon icon="x-mark" class="h-6 w-6 text-red-500 cursor-pointer" />
     </span>
   </div>
 </template>
