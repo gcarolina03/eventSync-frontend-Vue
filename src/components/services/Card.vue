aa717fba187e309a2b647fc933604ce34c27caac
Carolina Hernandez <carolinahernandez@cucunver.com>
2025-02-23 11:28:35 +0000

    refactor: limpiar código en componentes de mapa y tarjeta, mejorando la legibilidad y estructura

--- src/components/services/Card.vue
+++ src/components/services/Card.vue
@@ -2,8 +2,6 @@
 	<div
 		class="bg-white border border-gray-200 rounded-lg relative h-[200px] w-[150px] sm:h-[300px] sm:w-[200px] lg:h-[325px] lg:w-[225px] text-sm lg:text-base shadow-custom">
 		<div class="bg-[#9CC0FA] rounded-t-lg relative h-[180px]">
-
-
 			<Icon v-if="props.editMode" @handleClick="handleOpen" type="button" icon="pencil"
 				classButton="p-2.5 z-10 absolute top-[-10px] left-[-10px] text-sm font-bold bg-gray-300 hover:bg-gray-400 text-gray-700"
 				className="flex items-center justify-center h-3 w-3" />
@@ -11,30 +9,31 @@
 				$t('categoriesList.' + props.data.categoryId.title) }} </div>
 			<div
 				class="px-3 text-lg py-1 bg-neutral-100 text-black font-bold bottom-2 right-2 rounded-lg absolute opacity-80">{{
-					props.data.price }} €</div>
+				props.data.price }} €</div>
 			<img class="rounded-t-lg w-full h-full object-cover mx-auto" :src="props.data.img_url" alt="" />
 		</div>
-		<div class="p-5">
+		<div class="px-5 py-3	">
 			<h5 class="text-lg font-bold tracking-tight">{{ props.data.title }}</h5>
-			<div class="text-gray-600 mb-4">
+			<div class="text-gray-600 mb-1">
 				<!-- Dirección: se muestra si se obtuvo la ubicación -->
 				<span v-if="location && location.length > 0">
-					{{ location[6].long_name }} • {{ location[2].long_name }}
+					{{ location[6].long_name }} &nbsp; • &nbsp; {{ location[2].long_name }}
 				</span>
 				<!-- Icono de mapa con tooltip que muestra el mapa al hacer hover -->
-				<div v-if="props.data.latitude != undefined && props.data.longitude != undefined" class="group inline-flex cursor-pointer text-sm ml-2 relative text-gray-600 font-bold rounded-lg">
+				<div v-if="props.data.latitude != undefined && props.data.longitude != undefined"
+					class="group inline-flex cursor-pointer text-sm ml-2 relative text-gray-600 font-bold rounded-lg">
 					<Icon icon="map" class="h-3 w-3" />
 					<div class="opacity-0 flex items-center justify-center bg-white rounded-lg p-2 absolute group-hover:opacity-100 bottom-0 ml-8 pointer-events-none z-[10] w-52">
-						<Maps :latitude="props.data.latitude" :longitude="props.data.longitude" />
+						<Maps class="w-full" :latitude="props.data.latitude" :longitude="props.data.longitude" />
 					</div>
 				</div>
 			</div>
 			<div v-if="props.data.min_capacity && props.data.max_capacity" class="flex gap-2 items-center text-gray-600">
-				<Icon icon="user" />
+				<Icon icon="user" class="h-3 w-3" />
 				<p class="font-normal">from {{ props.data.min_capacity }} to {{ props.data.max_capacity }}</p>
 			</div>
 			<div v-if="props.data.start_time && props.data.end_time" class="flex gap-2 items-center text-gray-600">
-				<Icon icon="clock" />
+				<Icon icon="clock" class="h-3 w-3"	 />
 				<p class="font-normal">{{ props.data.start_time }}h - {{ props.data.end_time }}h</p>
 			</div>
 		</div>
@@ -44,13 +43,16 @@
 			{{ $t("servicesCard.request") }}
 		</div>
 		<div class="absolute bottom-3 left-5 flex gap-4">
-			<div @click="store.user && props.data.userId != store.user._id ? handleReviewService('up', props.data._id) : null" class="text-green-600 gap-1 font-bold flex text-lg items-center"
+			<div @click="store.user && props.data.userId != store.user._id ? handleReviewService('up', props.data._id) : null"
+				class="text-green-600 gap-1 font-bold flex text-lg items-center"
 				:class="[store.user && props.data.userId != store.user._id ? 'cursor-pointer' : 'cursor-default']">
 				<!-- Renderiza el ícono según si el usuario ya dio like -->
 				<Icon :icon="store.user && reviews.userUp ? 'thumbs-up-fill' : 'thumbs-up'" />
 				<span> {{ reviews.up }}</span>
 			</div>
-			<div @click="store.user && props.data.userId != store.user._id ? handleReviewService('down', props.data._id) : null" class="text-red-600 gap-1 font-bold flex text-lg items-center"
+			<div
+				@click="store.user && props.data.userId != store.user._id ? handleReviewService('down', props.data._id) : null"
+				class="text-red-600 gap-1 font-bold flex text-lg items-center"
 				:class="[store.user && props.data.userId != store.user._id ? 'cursor-pointer' : 'cursor-default']">
 				<!-- Renderiza el ícono según si el usuario ya dio dislike -->
 				<Icon :icon="store.user && reviews.userDown ? 'thumbs-down-fill' : 'thumbs-down'" />
@@ -80,8 +82,9 @@ const location = ref('')
 
 const emit = defineEmits(['openForm', 'closeForm', 'update', 'requestTo'])
 
-onBeforeMount(() => {
-	getAddress()
+onBeforeMount(async () => {
+	await getAddress()
+	console.log(props.data)
 })
 
 const handleOpen = () => {
